41411fd7af58b82fca5bab6ac0c0fe130270dad2
plipdra <philippe_nikos_edralin@dlsu.edu.ph>
2024-07-05 00:46:37 +0800

    cuda done

--- milestone 2/cudaHTP.cu
+++ milestone 2/cudaHTP.cu
@@ -1,57 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <string.h>
+#include <cuda_runtime.h>
 
 #define PI 3.14159265
 #define MAX_THETA 180
 
-// Function prototypes
-void houghTransform(unsigned char* image, int width, int height, int** accumulator, int* max_rho, int* num_thetas);
+__global__ void houghTransformKernel(unsigned char* d_image, int width, int height, int* d_accumulator, int max_rho, int num_thetas) {
+    int x = blockIdx.x * blockDim.x + threadIdx.x;
+    int y = blockIdx.y * blockDim.y + threadIdx.y;
+
+    if (x < width && y < height) {
+        if (d_image[y * width + x] > 0) {
+            for (int theta = 0; theta < num_thetas; ++theta) {
+                float theta_rad = (theta * PI) / 180.0;
+                int rho = (int)(x * cos(theta_rad) + y * sin(theta_rad));
+                int rho_index = rho + max_rho;  // Shift rho index to positive
+
+                if (rho_index >= 0 && rho_index < 2 * max_rho + 1) {
+                    atomicAdd(&d_accumulator[theta * (2 * max_rho + 1) + rho_index], 1);
+                }
+            }
+        }
+    }
+}
+
+void houghTransformCUDA(unsigned char* image, int width, int height, int** accumulator, int* max_rho, int* num_thetas) {
+    int* d_accumulator;
+    unsigned char* d_image;
+
+    *max_rho = (int)(sqrt(width * width + height * height));
+    *num_thetas = MAX_THETA;
+
+    size_t image_size = width * height * sizeof(unsigned char);
+    size_t accumulator_size = (*num_thetas) * (2 * (*max_rho) + 1) * sizeof(int);
+
+    *accumulator = (int*)calloc((*num_thetas) * (2 * (*max_rho) + 1), sizeof(int));
+
+    cudaMalloc((void**)&d_image, image_size);
+    cudaMalloc((void**)&d_accumulator, accumulator_size);
+    cudaMemcpy(d_image, image, image_size, cudaMemcpyHostToDevice);
+    cudaMemset(d_accumulator, 0, accumulator_size);
+
+    dim3 blockSize(16, 16);
+    dim3 gridSize((width + blockSize.x - 1) / blockSize.x, (height + blockSize.y - 1) / blockSize.y);
+    houghTransformKernel<<<gridSize, blockSize>>>(d_image, width, height, d_accumulator, *max_rho, *num_thetas);
+
+    cudaMemcpy(*accumulator, d_accumulator, accumulator_size, cudaMemcpyDeviceToHost);
+
+    cudaFree(d_image);
+    cudaFree(d_accumulator);
+}
+
 void saveAccumulatorAsPGM(int* accumulator, int max_rho, int num_thetas, const char* filename);
 
 int main() {
     printf("Start\n");
-    // Example image dimensions (replace with actual values)
-    int width64 = 8;
-    int height64 = 8;
-
-    int width256 = 16;
-    int height256 = 16;
-
     int width = 32;
     int height = 32;
-
-    // Example edge-detected image (replace with actual edge-detected image)
-    unsigned char image64[64] = {
-        1, 0, 0, 0, 0, 0, 0, 0,
-        0, 1, 0, 0, 0, 0, 0, 0,
-        0, 0, 1, 0, 0, 0, 0, 0,
-        0, 0, 0, 1, 0, 0, 0, 0,
-        0, 0, 0, 0, 1, 0, 0, 0,
-        0, 0, 0, 0, 0, 1, 0, 0,
-        0, 0, 0, 0, 0, 0, 1, 0,
-        0, 0, 0, 0, 0, 0, 0, 1
-    };
-
-    unsigned char image256[256] = {
-        1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0,
-        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1
-    };
 
     unsigned char image[1024] = {
         1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
@@ -88,46 +94,16 @@ int main() {
         0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1
     };
 
-    // Hough Transform variables
     int* accumulator;
     int max_rho;
     int num_thetas;
 
-    // Perform Hough Transform
-    houghTransform(image, width, height, &accumulator, &max_rho, &num_thetas);
+    houghTransformCUDA(image, width, height, &accumulator, &max_rho, &num_thetas);
 
-    // Save the accumulator array as an image
-    saveAccumulatorAsPGM(accumulator, max_rho, num_thetas, "accumulator1024.pgm");
+    saveAccumulatorAsPGM(accumulator, max_rho, num_thetas, "accumulator1024_cuda.pgm");
 
     free(accumulator);
     return 0;
-}
-
-void houghTransform(unsigned char* image, int width, int height, int** accumulator, int* max_rho, int* num_thetas) {
-    int x, y, theta;
-    int rho;
-    float theta_rad;
-
-    *max_rho = (int)(sqrt(width * width + height * height));
-    *num_thetas = MAX_THETA;
-
-    *accumulator = (int*)calloc((*num_thetas) * (2 * (*max_rho) + 1), sizeof(int));
-
-    for (y = 0; y < height; ++y) {
-        for (x = 0; x < width; ++x) {
-            if (image[y * width + x] > 0) {
-                for (theta = 0; theta < *num_thetas; ++theta) {
-                    theta_rad = (theta * PI) / 180.0;
-                    rho = (int)(x * cos(theta_rad) + y * sin(theta_rad));
-                    int rho_index = rho + *max_rho;  // Shift rho index to positive
-
-                    if (rho_index >= 0 && rho_index < 2 * (*max_rho) + 1) {
-                        (*accumulator)[theta * (2 * (*max_rho) + 1) + rho_index]++;
-                    }
-                }
-            }
-        }
-    }
 }
 
 void saveAccumulatorAsPGM(int* accumulator, int max_rho, int num_thetas, const char* filename) {
@@ -158,3 +134,4 @@ void saveAccumulatorAsPGM(int* accumulator, int max_rho, int num_thetas, const c
 
     fclose(file);
 }
+
